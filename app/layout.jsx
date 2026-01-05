535e1694e64a00de0fd32ad742d2af22f00b99f3
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-05 15:33:58 -0500

    Add toggle for light and dark mode

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import { ThemeToggle } from '../components/theme-toggle';
 import { Footer } from '../components/footer'
 
 export default function RootLayout({ children }) {
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
             <body
                 className="antialiased transition-colors" 
                 style={{ backgroundColor: 'var(--bg-color)', color: 'var(--text-color)' }}>
+                <ThemeToggle />
                 <div className="flex flex-col min-h-screen px-6 sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <main className="grow">{children}</main>
