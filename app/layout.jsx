444566635361d724212b469e9f093896d9c8d7a5
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-05 15:29:36 -0500

    Add name and contact information

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,16 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                <link rel="icon" href="null" sizes="any" />
             </head>
-            <body>
+            <body
+                className="antialiased transition-colors" 
+                style={{ backgroundColor: 'var(--bg-color)', color: 'var(--text-color)' }}>
+                <div className="flex flex-col min-h-screen px-6 sm:px-12">
+                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
+                        <main className="grow">{children}</main>
+                    </div>
+                </div>
             </body>
         </html>
     );
