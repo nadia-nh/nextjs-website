79398c5726ddbe0c50482a022bf66840fdb105ee
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-05 15:37:27 -0500

    Add header with home and project sections

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import { ThemeToggle } from '../components/theme-toggle';
+import { Header } from '../components/header';
 import { Footer } from '../components/footer'
 
 export default function RootLayout({ children }) {
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
                 <ThemeToggle />
                 <div className="flex flex-col min-h-screen px-6 sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
+                        <Header />
                         <main className="grow">{children}</main>
                         <Footer />
                     </div>
--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,29 @@
+import Link from 'next/link';
+import { Home, FolderOpen } from 'lucide-react';
+
+const navItems = [
+    { linkText: 'About', href: '/', icon: Home },
+    { linkText: 'Projects', href: '/', icon: FolderOpen },
+];
+
 export function Header() {
     return (
-        <div>
-        </div>
+        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
+            {!!navItems?.length && (
+                <ul className="flex flex-wrap gap-x-4 gap-y-1">
+                    {navItems.map((item, index) => (
+                        <li key={index}>
+                            <Link href={item.href} className="inline-flex items-center gap-1.5 px-1.5 py-1 sm:px-3 sm:py-2">
+                                {item.icon ? (
+                                    <item.icon size={18} aria-label={item.linkText} />
+                                ) : (
+                                    item.linkText
+                                )}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </nav>
     );
 }
