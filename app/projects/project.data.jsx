8a5cfb078aa167147e6a0092804f0e80f73b7841
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-16 00:17:32 -0500

    Add demo links for most projects

--- app/projects/project.data.jsx
+++ app/projects/project.data.jsx
@@ -28,7 +28,7 @@ export const games = [
         title: 'RC Pico-8 Shoot Em Up Game',
         description: 'Small shoot em up game made with Pico-8. The player controls a ship that can move and shoot lasers at falling enemies.',
         image: screenshotRcShooter,
-        liveUrl: null,
+        liveUrl: 'https://www.lexaloffle.com/bbs/cart_info.php?cid=rasifebuhi-8',
         githubUrl: 'https://github.com/nadia-nh/rc-pico8-shoot-em-up',
         tags: ['Pico-8', 'lua', 'shmup', 'recurse-center', 'solo']
     },
@@ -37,16 +37,16 @@ export const games = [
         title: 'RC Love2D Platformer Game',
         description: 'Small platformer game made with Löve-2D. The player can jump over platforms in order to collect coins.',
         image: screenshotRcLovePlatformer,
-        liveUrl: null,
+        liveUrl: 'https://forgepixel.itch.io/rc-love2d-platformer',
         githubUrl: 'https://github.com/nadia-nh/rc-love2d-platformer',
         tags: ['Löve-2D', 'lua', 'platformer', 'recurse-center', 'solo']
     },
     {
         id: 4,
-        title: 'RC Godot Survivors-like Game',
+        title: 'RC Godot Underwater Survivors Game',
         description: 'Small underwater survivors-like game made with Godot. The player can move around and avoid enemies.',
         image: screenshotRcSurvivors,
-        liveUrl: null,
+        liveUrl: 'https://forgepixel.itch.io/underwater-survivors',
         githubUrl: 'https://github.com/nadia-nh/rc-survivors-game',
         tags: ['Godot', 'gdscript', 'survivors-like', 'recurse-center', 'team']
     },
@@ -55,7 +55,7 @@ export const games = [
         title: 'RC RCade 8-ball Game',
         description: 'Small RCade game that simulates a magic 8-ball. The player can ask a question and receive a random answer.',
         image: screenshotRc8Ball,
-        liveUrl: null,
+        liveUrl: 'https://editor.p5js.org/nadia-nh/full/VKEy3PDBB',
         githubUrl: 'https://github.com/nadia-nh/rc-8-ball-rcade',
         tags: ['RCade', 'javascript', 'recurse-center', 'team']
     },
@@ -64,7 +64,7 @@ export const games = [
         title: 'RC RCade Cat Ignore Game',
         description: 'Small RCade game featuring a cat. The player calls out to it to try to get its attention.',
         image: screenshotRcCatIgnore,
-        liveUrl: null,
+        liveUrl: 'https://editor.p5js.org/nadia-nh/full/lWcGcg2eZ',
         githubUrl: 'https://github.com/sllewely/cat-ignore',
         tags: ['RCade', 'typescript', 'recurse-center', 'team']
     },
@@ -76,7 +76,7 @@ export const creative_code = [
         title: 'RC Flower Garden',
         description: 'Left click anywhere to plant a random flower, right click to remove them. The flowers have a slight left to right sway.',
         image: screenshotRcFlowerGarden,
-        liveUrl: null,
+        liveUrl: 'https://editor.p5js.org/nadia-nh/full/H0Lgt_z6k',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/flower-garden',
         tags: ['p5.js', 'javascript', 'garden', 'recurse-center', 'solo']
     },
@@ -85,7 +85,7 @@ export const creative_code = [
         title: 'RC Path Maker',
         description: 'Click to change the current tile, they cycle through grass, soil, water, rock and grass. Clicking and dragging changes multiple tiles at once.',
         image: screenshotRcPathMaker,
-        liveUrl: null,
+        liveUrl: 'https://editor.p5js.org/nadia-nh/full/2wc-SBZAT',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/path-maker',
         tags: ['p5.js', 'javascript', 'recurse-center', 'solo']
     },
@@ -94,7 +94,7 @@ export const creative_code = [
         title: 'RC Flower Pomodoro',
         description: 'Click the "Plant" button to start. Click the timer to increase the time, starting from 5 to 60 minutes, in 5 minute increments.',
         image: screenshotRcFlowerPomodoro,
-        liveUrl: null,
+        liveUrl: 'https://editor.p5js.org/nadia-nh/full/NZ9HCfpnp',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/flower-pomodoro',
         tags: ['p5.js', 'javascript', 'tracking', 'recurse-center', 'solo']
     },
@@ -103,7 +103,7 @@ export const creative_code = [
         title: 'RC Rainbow Spiral',
         description: 'Moving spiral cycling through colors as it spins. The colors depend on the distance of each pixel to the center. The spiral fades at the edge.',
         image: screenshotRcRainbowSpiral,
-        liveUrl: null,
+        liveUrl: 'https://www.shadertoy.com/view/33SBDD',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/rainbow-spiral',
         tags: ['shadertoy', 'glsl', 'recurse-center', 'solo']
     },
@@ -112,7 +112,7 @@ export const creative_code = [
         title: 'RC Rings And Planets',
         description: 'Moving rings with tiny orbiting planets on them, with dynamic motion and a background color gradient. Based on another shader made collaboratively, this version adapts the drawing logic and adds color.',
         image: screenshotRcRingsPlanets,
-        liveUrl: null,
+        liveUrl: 'https://www.shadertoy.com/view/W3jBWW',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/rings-and-planets',
         tags: ['shadertoy', 'glsl', 'recurse-center', 'solo']
     },
@@ -121,7 +121,7 @@ export const creative_code = [
         title: 'RC Shifting Spheres',
         description: 'Two overlapping circles with eye-like inner pupils that shift and move over time, with color gradients. Based on functions from the pixel spirit deck.',
         image: screenshotRcShiftingSpheres,
-        liveUrl: null,
+        liveUrl: 'https://www.shadertoy.com/view/tc3cWN',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/shifting-spheres',
         tags: ['shadertoy', 'glsl', 'recurse-center', 'team']
     },
