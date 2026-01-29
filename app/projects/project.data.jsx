c919c3b3a287b593308e0c9aabebe1fb63dc118a
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-29 15:50:20 -0500

    Update project descriptions to be more cohesive.
    They all use second person now and are clearer. Also updates the tags.

--- app/projects/project.data.jsx
+++ app/projects/project.data.jsx
@@ -19,20 +19,22 @@ export const applications = [
     {
         id: 1,
         title: 'Quiet Garden',
-        description: 'Pomodoro app built with Android and Jetpack Compose. The user can set a timer and track their focus sessions.',
+        description: `Pomodoro app built with Android.
+            Set a customizable timer to focus and track you pomodoro sessions.`,
         image: screenshotRcPomodoro,
         liveUrl: null,
         githubUrl: 'https://github.com/nadia-nh/rc-android-pomodoro',
-        tags: ['Android', 'jetpack-compose', 'room', 'recurse-center', 'solo']
+        tags: ['Android', 'Jetpack-compose', 'Room', 'Productivity', 'Recurse-center', 'Solo']
     },
     {
         id: 2,
         title: 'Daily Drip',
-        description: 'Water tracking app built with React and Nextjs. The user can log their daily water intake and view their progress over time.',
+        description: `Water tracking app built with React.
+            Log your daily water intake and view your progress over time.`,
         image: screenshotRcWaterTracking,
         liveUrl: 'https://rc-water-tracking-app.vercel.app/',
         githubUrl: 'https://github.com/nadia-nh/rc-water-tracking-app',
-        tags: ['React', 'Nextjs', 'typescript', 'recurse-center', 'team']
+        tags: ['React', 'Nextjs', 'Typescript', 'Productivity', 'Recurse-center', 'Team']
     },
 ];
 
@@ -40,56 +42,62 @@ export const games = [
     {
         id: 1,
         title: 'Plant And Prosper',
-        description: 'Farming game made with Godot Engine. The player can prepare the soil, plant seeds, water crops, and harvest them once they are fully grown.',
+        description: `Cozy farming game made with Godot.
+            Prepare the soil, plant seeds, water crops, and sell them once they are fully grown.`,
         image: screenshotRcGodotFarmRpg,
         liveUrl: 'https://forgepixel.itch.io/plant-and-prosper',
         githubUrl: 'https://github.com/nadia-nh/rc-godot-farm-rpg',
-        tags: ['Godot', 'gdscript', 'farming', 'recurse-center', 'solo']
+        tags: ['Godot', 'GDScript', 'Cozy', 'Recurse-center', 'Solo']
     },
     {
         id: 2,
         title: 'Space Laser',
-        description: 'Small shoot em up game made with Pico-8. The player controls a ship that can move and shoot lasers at falling enemies.',
+        description: `Shoot-em-up game made with Pico-8.
+            Steer a ship and shoot lasers at falling enemies.`,
         image: screenshotRcShooter,
         liveUrl: 'https://www.lexaloffle.com/bbs/cart_info.php?cid=rasifebuhi-8',
         githubUrl: 'https://github.com/nadia-nh/rc-pico8-shoot-em-up',
-        tags: ['Pico-8', 'lua', 'shmup', 'recurse-center', 'solo']
+        tags: ['Pico-8', 'Lua', 'SHMUP', 'Recurse-center', 'Solo']
     },
     {
         id: 3,
         title: 'One More Jump',
-        description: 'Small platformer game made with Löve-2D. The player can jump over platforms in order to collect coins.',
+        description: `Platformer game made with Löve-2D. 
+            Jump over platforms to collect all the coins.`,
         image: screenshotRcLovePlatformer,
         liveUrl: 'https://forgepixel.itch.io/one-more-jump',
         githubUrl: 'https://github.com/nadia-nh/rc-love2d-platformer',
-        tags: ['Löve-2D', 'lua', 'platformer', 'recurse-center', 'solo']
+        tags: ['Löve-2D', 'Lua', 'Platformer', 'Recurse-center', 'Solo']
     },
     {
         id: 4,
         title: 'Fishy Water',
-        description: 'Small underwater survivors-like game made with Godot. The player can move around and avoid enemies.',
+        description: `Underwater survivors game experience made with Godot.
+            Swim as fast as you can to avoid oncoming enemies.`,
         image: screenshotRcSurvivors,
         liveUrl: 'https://forgepixel.itch.io/fishy-water',
         githubUrl: 'https://github.com/nadia-nh/rc-survivors-game',
-        tags: ['Godot', 'gdscript', 'survivors-like', 'recurse-center', 'team']
+        tags: ['Godot', 'GDScript', 'Survivors', 'Recurse-center', 'Team']
     },
     {
         id: 5,
         title: 'Ask The Orb',
-        description: 'Small RCade game that simulates a magic 8-ball. The player can ask a question and receive a random answer.',
+        description: `RCade game simulating a magic 8-ball.
+            Ask a yes-no question and receive your answer from the all knowing 8-ball.`,
         image: screenshotRc8Ball,
         liveUrl: 'https://editor.p5js.org/nadia-nh/full/VKEy3PDBB',
         githubUrl: 'https://github.com/nadia-nh/rc-8-ball-rcade',
-        tags: ['RCade', 'javascript', 'recurse-center', 'team']
+        tags: ['RCade', 'Javascript', 'Recurse-center', 'Team']
     },
     {
         id: 6,
         title: 'Your Cat',
-        description: 'Small RCade game featuring a cat. The player calls out to it to try to get its attention.',
+        description: `RCade cozy game featuring a cat.
+            Call out to your cat to get its attention, good luck!`,
         image: screenshotRcCatIgnore,
         liveUrl: 'https://editor.p5js.org/nadia-nh/full/lWcGcg2eZ',
         githubUrl: 'https://github.com/sllewely/cat-ignore',
-        tags: ['RCade', 'typescript', 'recurse-center', 'team']
+        tags: ['RCade', 'Typescript', 'Cozy', 'Recurse-center', 'Team']
     },
 ];
 
@@ -97,55 +105,65 @@ export const creative_code = [
     {
         id: 1,
         title: 'Flower Garden',
-        description: 'Left click anywhere to plant a random flower, right click to remove them. The flowers have a slight left to right sway.',
+        description: `Cozy garden made in p5.js.
+            Left-click to plant flowers and watch them sway in the breeze.
+            Right-click to remove them.`,
         image: screenshotRcFlowerGarden,
         liveUrl: 'https://editor.p5js.org/nadia-nh/full/H0Lgt_z6k',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/flower-garden',
-        tags: ['p5.js', 'javascript', 'garden', 'recurse-center', 'solo']
+        tags: ['P5.js', 'Javascript', 'Cozy', 'Recurse-center', 'Solo']
     },
     {
         id: 2,
         title: 'Path Maker',
-        description: 'Click to change the current tile, they cycle through grass, soil, water, rock and grass. Clicking and dragging changes multiple tiles at once.',
+        description: `Path drawing experience made in p5.js.
+            Click to change tiles, cycling through grass, soil, water, rock and grass.
+            Click and drag to change tiles continuously.`,
         image: screenshotRcPathMaker,
         liveUrl: 'https://editor.p5js.org/nadia-nh/full/2wc-SBZAT',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/path-maker',
-        tags: ['p5.js', 'javascript', 'recurse-center', 'solo']
+        tags: ['P5.js', 'Javascript', 'Recurse-center', 'Solo']
     },
     {
         id: 3,
         title: 'Flower Pomodoro',
-        description: 'Click the "Plant" button to start. Click the timer to increase the time, starting from 5 to 60 minutes, in 5 minute increments.',
+        description: `Flower themed pomodoro made in p5.js.
+            Click the Plant button to grow a flower.
+            Click the timer to change the duration.`,
         image: screenshotRcFlowerPomodoro,
         liveUrl: 'https://editor.p5js.org/nadia-nh/full/NZ9HCfpnp',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/flower-pomodoro',
-        tags: ['p5.js', 'javascript', 'tracking', 'recurse-center', 'solo']
+        tags: ['P5.js', 'Javascript', 'Productivity', 'Recurse-center', 'Solo']
     },
     {
         id: 4,
         title: 'Rainbow Spiral',
-        description: 'Moving spiral cycling through colors as it spins. The colors depend on the distance of each pixel to the center. The spiral fades at the edge.',
+        description: `Rainbow spiral using shaders.
+            The spiral cycles through colors as it spins and fades at the edge.`,
         image: screenshotRcRainbowSpiral,
         liveUrl: 'https://www.shadertoy.com/view/33SBDD',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/rainbow-spiral',
-        tags: ['shadertoy', 'glsl', 'recurse-center', 'solo']
+        tags: ['Shaders', 'GLSL', 'Recurse-center', 'Solo']
     },
     {
         id: 5,
         title: 'Rings And Planets',
-        description: 'Moving rings with tiny orbiting planets on them, with dynamic motion and a background color gradient. Based on another shader made collaboratively, this version adapts the drawing logic and adds color.',
+        description: `Constellation using shaders.
+            The rings with orbiting planets move dynamically.
+            The colors shift with time.`,
         image: screenshotRcRingsPlanets,
         liveUrl: 'https://www.shadertoy.com/view/W3jBWW',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/rings-and-planets',
-        tags: ['shadertoy', 'glsl', 'recurse-center', 'solo']
+        tags: ['Shaders', 'GLSL', 'Recurse-center', 'Solo']
     },
     {
         id: 6,
         title: 'Shifting Spheres',
-        description: 'Two overlapping circles with eye-like inner pupils that shift and move over time, with color gradients. Based on functions from the pixel spirit deck.',
+        description: `Eye like circles using shaders.
+            Shifting spheres move over time, featuring a changing color gradient.`,
         image: screenshotRcShiftingSpheres,
         liveUrl: 'https://www.shadertoy.com/view/tc3cWN',
         githubUrl: 'https://github.com/nadia-nh/rc-creative-coding/tree/main/shifting-spheres',
-        tags: ['shadertoy', 'glsl', 'recurse-center', 'team']
+        tags: ['Shaders', 'GLSL', 'Recurse-center', 'Team']
     },
 ];
