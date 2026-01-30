b2b13fa386dc89a986f5ba47bd08f4b39c86137e
nadia-nh <nadia.heredia.nh@gmail.com>
2026-01-30 18:30:44 -0500

    Add links to the game engines and frameworks in the game projects site

--- app/game-projects/page.jsx
+++ app/game-projects/page.jsx
@@ -21,7 +21,14 @@ export default function GameProjectsPage() {
                 </p>
             </div>
 
-            <h2 className="mb-4">Godot Engine</h2>
+            <h2 className="mb-4">
+                <a
+                    href='https://godotengine.org/'
+                    target="_blank"
+                    aria-label="Godot Engine website">
+                    Godot Engine
+                </a>
+            </h2>
             {/* Godot Games Grid */}
             <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
                 {games_godot.map((game) => (
@@ -30,26 +37,40 @@ export default function GameProjectsPage() {
             </div>
 
             <br /><br />
-            <h2 className="mb-4">
+            <h2 className="mb-4" style={{ display: 'flex', gap: '10px', alignItems: 'left' }}>
                 <a
-                href='https://github.com/fcjr/RCade'
-                target="_blank"
-                aria-label="RCade repository">
-                RCade
+                    href='https://www.lexaloffle.com/pico-8.php'
+                    target="_blank"
+                    aria-label="Pico-8 fantasy console website">
+                    Pico-8
+                </a>
+                and
+                <a
+                    href='https://love2d.org/'
+                    target="_blank"
+                    aria-label="Löve-2D framework website">
+                    Löve-2D
                 </a>
             </h2>
-            {/* RCade Games Grid */}
+            {/* Other Games Grid */}
             <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-                {games_rcade.map((game) => (
+                {games_other.map((game) => (
                     <ProjectCard key={game.id} project={game} />
                 ))}
             </div>
 
             <br /><br />
-            <h2 className="mb-4">Other Engines</h2>
-            {/* Other Games Grid */}
+            <h2 className="mb-4">
+                <a
+                    href='https://github.com/fcjr/RCade'
+                    target="_blank"
+                    aria-label="RCade repository">
+                    RCade
+                </a>
+            </h2>
+            {/* RCade Games Grid */}
             <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-                {games_other.map((game) => (
+                {games_rcade.map((game) => (
                     <ProjectCard key={game.id} project={game} />
                 ))}
             </div>
